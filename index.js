d9dd3edf8b90072b7b4cc2d23fb3aeb040c01bb0
nima shahbazi <nimashahbazi524@gmail.com>
2021-02-17 04:09:36 -0800

    Add connection and disconnection feature by socket.io

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // Modules
 const path = require("path");
 const express = require("express");
+const socketIO = require("socket.io");
+const http = require("http");
 
 // Define const variables
 const publicPath = path.join(__dirname, "/public");
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server)
 
+io.on("connection", socket => {
+    console.log("New user connected.")
+});
+io.on("disconnect", () => {
+    console.log("user was disconnected.")
+})
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`app is started on port ${port}...`))
+// Run project on server
+server.listen(port, () => console.log(`app is started on port ${port}...`))
 
 
